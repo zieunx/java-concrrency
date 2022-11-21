1110ee3559a3cab571b41d666a86c820be4414e2
zieunx <wldms_1217@naver.com>
2022-11-21 22:14:14 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/study/concurrency/JavaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
